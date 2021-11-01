9c32f89537322c6343e7ad0bc47dc4b05b693b2f
Arivazhagan <arivu.akil@yahoo.com>
2021-11-01 19:05:38 +0530

    updated

---
+++ Error.java
@@ -0,0 +1,14 @@
+public class Error
+{
+static int no = 5;
+public static void main(String[] args)
+{
+Error e = new Error();
+System.out.println("Welcome to Error Program");
+System.out.println(no);
+System.out.println("Hi");
+	
+	
+}
+	
+}

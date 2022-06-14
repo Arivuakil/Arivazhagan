1392addc657d8c77a46cdaaf48a0ab61444d18c1
Arivazhagan <arivu.akil@yahoo.com>
2022-06-14 10:31:30 +0530

    Today update

---
+++ Phone.java
@@ -0,0 +1,17 @@
+package nineties;
+public class Phone
+{
+	public int price = 600;
+	
+public static void main(String [] args)
+{
+}
+public void fixPrice()
+{
+System.out.println("1000");
+}
+public void call()
+{ 
+System.out.println("Incoming");
+}
+}
